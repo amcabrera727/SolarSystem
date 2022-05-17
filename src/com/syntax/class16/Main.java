87959a245dcd4465630d7abe80e2c920f284c316
Anna Everyday <amcabrera727@gmail.com>
2022-05-17 19:17:01 -0400

    Solar System Basic Project Ready

---
+++ src/com/syntax/class16/Main.java
@@ -0,0 +1,15 @@
+package com.syntax.class16;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        SolarSystem FactorX= new SolarSystem();
+        FactorX.Sun="1";
+        FactorX.Moon="4";
+        FactorX.Star="2";
+        FactorX.Planets="10000";
+
+    }
+}
---
+++ src/com/syntax/class16/SolarSystem.java
@@ -0,0 +1,11 @@
+package com.syntax.class16;
+
+public class SolarSystem {
+
+    String Sun;
+    String Moon;
+    String Star;
+    String Planets;
+
+
+}
